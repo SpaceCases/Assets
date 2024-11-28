dae48bcc28304773b95dd49f452391168ebb8a4b
William Redding <williamdredding@proton.me>
2024-11-28 10:54:48 +0000

    Move scripts into src directory, update README

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Assets
+# SpaceCases Assets
 
-This repository contains static assets and asset generation scripts for SpaceCases
+This repository contains scripts and static assets used for generating and serving item skins and related data for SpaceCases.
+
+## Overview
+
+This scripts in this repository generate:
+1. Images for item skins (`gen_skin_images.py`).
+2. Item skin data (`gen_skin_data.py`)
+
+
+## Setup
+
+```bash
+git clone https://github.com/SpaceCases/Assets    # Clone the repository
+cd Assets                                         # Move into the repository directory
+python -m venv env                                # Create virtual environment
+source env/bin/activate                           # Activate virtual environment
+python -m pip install -r requirements.txt         # Install dependencies
+python src/gen_skin_images.py                     # Generate images for items
+python src/gen_skin_data.py                       # Generate item data JSON file
+```
+Then use whatever scheduling system you like to periodically run the `refresh_prices.py` script to refresh the item prices in `skin_data.json`. The `assets` folder can then be served using any HTTP server.
--- src/gen_skin_data.py
+++ src/gen_skin_data.py
@@ -29,7 +29,8 @@ def run(api_data) -> dict[str, Skin]:
         if max_float is None:
             max_float = 1.0
         # all available conditions
-        available_conditions = get_all_conditions_for_float_range(min_float, max_float)
+        available_conditions = get_all_conditions_for_float_range(
+            min_float, max_float)
         for condition in available_conditions:
             # if its a doppler include its phase in the name
             if "Doppler" in formatted_name:
@@ -38,8 +39,10 @@ def run(api_data) -> dict[str, Skin]:
             else:
                 full_formatted_name = f"{formatted_name} ({condition})"
             # the flavour text is between the italic html tags
-            full_unformatted_name = remove_skin_name_formatting(full_formatted_name)
-            description_match = re.search(r"<i>(.*?)</i>", datum["description"])
+            full_unformatted_name = remove_skin_name_formatting(
+                full_formatted_name)
+            description_match = re.search(r"<i>(.*?)</i>",
+                                          datum["description"])
             if description_match:
                 description = description_match.group(1)
             else:
@@ -65,13 +68,11 @@ def run(api_data) -> dict[str, Skin]:
             if datum.get("stattrak", False):
                 if "★" in full_formatted_name:
                     stattrak_full_formatted_name = full_formatted_name.replace(
-                        "★", "★ StatTrak™"
-                    )
+                        "★", "★ StatTrak™")
                 else:
                     stattrak_full_formatted_name = f"StatTrak™ {full_formatted_name}"
                 stattrak_full_unformatted_name = remove_skin_name_formatting(
-                    stattrak_full_formatted_name
-                )
+                    stattrak_full_formatted_name)
                 stattrak_skin_datum = dataclasses.replace(skin_datum)
                 stattrak_skin_datum.formatted_name = stattrak_full_formatted_name
                 stattrak_skin_datum.image_url = os.path.join(
@@ -85,8 +86,7 @@ def run(api_data) -> dict[str, Skin]:
             if datum.get("souvenir", False):
                 souvenir_full_formatted_name = f"Souvenir {full_formatted_name}"
                 souvenir_full_unformatted_name = remove_skin_name_formatting(
-                    souvenir_full_formatted_name
-                )
+                    souvenir_full_formatted_name)
                 souvenir_skin_datum = dataclasses.replace(skin_datum)
                 souvenir_skin_datum.formatted_name = souvenir_full_formatted_name
                 souvenir_skin_datum.image_url = os.path.join(
@@ -100,10 +100,11 @@ def run(api_data) -> dict[str, Skin]:
 
 
 if __name__ == "__main__":
+    os.makedirs(OUTPUT_DIRECTORY, exist_ok=True)
     api_data = requests.get(
-        "https://bymykel.github.io/CSGO-API/api/en/skins.json"
-    ).json()
+        "https://bymykel.github.io/CSGO-API/api/en/skins.json").json()
     result = run(api_data)
     out_result = {key: asdict(value) for key, value in result.items()}
-    with open(f"{OUTPUT_DIRECTORY}/skin_data.json", "w+", encoding="utf-8") as f:
+    with open(f"{OUTPUT_DIRECTORY}/skin_data.json", "w+",
+              encoding="utf-8") as f:
         json.dump(out_result, f, ensure_ascii=False, indent=4)
